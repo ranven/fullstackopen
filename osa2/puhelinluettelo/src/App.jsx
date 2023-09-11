c2af16ba7df15687203f2cdf8fee47c15a64c12d
venla <venla.e.rantanen@gmail.com>
2023-09-11 15:12:54 +0300

    Task 3.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/persons").then((res) => {
+    axios.get("http://localhost:3001/api/persons").then((res) => {
       setPersons(res.data);
     });
   }, []);
