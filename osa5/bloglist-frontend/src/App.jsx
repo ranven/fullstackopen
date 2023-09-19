8dd33f984779e5a6f9c96973beab799fb199c536
venla <venla.e.rantanen@gmail.com>
2023-09-19 11:29:30 +0300

    Task 5.1

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,21 +1,88 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react"
+import Blog from "./components/Blog"
+import { Notification } from "./components/Notification"
+
+import blogService from "./services/blogs"
+import loginService from "./services/login"
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState("")
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername("")
+      setPassword("")
+    } catch (exception) {
+      displayNotification("wrong credentials", true)
+    }
+  }
+
+  const displayNotification = (message, isError) => {
+    setError(isError)
+    setNotification(message)
+    setTimeout(() => {
+      setNotification("")
+      setError(false)
+    }, 4000)
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>Blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <h1>Blogs</h1>
+
+      <Notification message={notification} isError={error}></Notification>
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.username} logged in</p>
+          {blogList()}
+        </div>
       )}
     </div>
   )
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,19 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios"
+const baseUrl = "/api/blogs"
+let token = null
 
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return request.then((response) => response.data)
 }
 
-export default { getAll }
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
+}
+
+const createBlog = async (newBlog) => {
+  const config = { headers: { Authorization: token } }
+  const res = await axios.post(baseUlr, newBlog, config)
+}
+
+export default { getAll, setToken, createBlog }
