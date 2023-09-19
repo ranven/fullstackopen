d462ba843d8b8f33ddb83413d3ed3d58c3ff20e2
venla <venla.e.rantanen@gmail.com>
2023-09-19 14:13:46 +0300

    Task 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div className="blog-box">
+      <div>
+        <p>
+          {blog.title}
+          <button onClick={toggleVisible} style={{ marginLeft: ".5rem" }}>
+            {visible ? "hide" : "view"}
+          </button>
+        </p>
+      </div>
+      <div style={{ display: visible ? "" : "none" }}>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes} likes <button onClick={console.log("like")}>like</button>
+        </p>
+        <p>{blog.author}</p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
