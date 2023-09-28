5684a3c5096c13cf4522bd3a3389ce261fd00c3e
venla <venla.e.rantanen@gmail.com>
2023-09-28 14:52:08 +0300

    Task 6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,9 +2,15 @@ import { useDispatch, useSelector } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) =>
-    state.sort((a, b) => b.votes - a.votes)
-  )
+  const anecdotes = useSelector((state) => {
+    if (state.filter && state.filter.length > 1) {
+      return state.anecdotes
+        .filter((a) => a.content.includes(state.filter))
+        .sort((a, b) => b.votes - a.votes)
+    }
+    return state.anecdotes.sort((a, b) => b.votes - a.votes)
+  })
+
   const dispatch = useDispatch()
 
   const vote = (id) => {
