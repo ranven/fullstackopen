5d762934c89fb29211dec824e8c142341be6db88
venla <venla.e.rantanen@gmail.com>
2023-09-28 13:40:04 +0300

    Tasks 6.5-6.8

---
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -0,0 +1,26 @@
+import { useDispatch } from "react-redux"
+import { newAnecdote } from "../reducers/anecdoteReducer"
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addNew = (event) => {
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ""
+    dispatch(newAnecdote(anecdote))
+  }
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addNew}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
