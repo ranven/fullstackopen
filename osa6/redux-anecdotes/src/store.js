96f20e95073ff5b3db1069fde066f6013293372a
venla <venla.e.rantanen@gmail.com>
2023-09-29 16:09:58 +0300

    (Task 6.14) Initialize anecdotes in app.js

--- osa6/redux-anecdotes/src/store.js
+++ osa6/redux-anecdotes/src/store.js
@@ -1,8 +1,7 @@
 import filterReducer from "./reducers/filterReducer"
-import anecdoteReducer, { setAnecdotes } from "./reducers/anecdoteReducer"
+import anecdoteReducer from "./reducers/anecdoteReducer"
 import { configureStore } from "@reduxjs/toolkit"
 import notificationReducer from "./reducers/notificationReducer"
-import anecdoteService from "./services/anecdotes"
 
 const store = configureStore({
   reducer: {
@@ -10,10 +9,6 @@ const store = configureStore({
     filter: filterReducer,
     notification: notificationReducer,
   },
-})
-
-anecdoteService.getAll().then((anecdotes) => {
-  store.dispatch(setAnecdotes(anecdotes))
 })
 
 export default store
