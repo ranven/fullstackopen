e271728f95ed4170978c8a3417457fddb828c0b7
venla <venla.e.rantanen@gmail.com>
2023-10-02 12:34:45 +0300

    Task 6.20

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,38 +1,42 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm"
+import Notification from "./components/Notification"
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
+import { getAnecdotes } from "./requests"
 
 const App = () => {
-
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log("vote")
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+  })
+
+  if (result.isLoading) {
+    return <div>Loading...</div>
+  } else if (result.isError) {
+    return <div>Anecdote service not available due to server problems</div>
+  }
+  const anecdotes = result.data
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
   )
 }
