e4f7d9ba5a9de2d795a53bc80acd3c13285f558c
venla <venla.e.rantanen@gmail.com>
2023-10-12 14:19:22 +0300

    Task 8.6

--- osa8/index.js
+++ osa8/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server")
 const { startStandaloneServer } = require("@apollo/server/standalone")
+const { v1: uuid } = require("uuid")
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -140,6 +150,20 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      if (!authors.find((a) => a.name === book.author)) {
+        let newAuthor = {
+          name: book.author,
+          id: uuid(),
+        }
+        authors = authors.concat(newAuthor)
+      }
+      books = books.concat(book)
+      return book
+    },
   },
 }
 
