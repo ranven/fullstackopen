6c75fb3fe84ab0e7c345436dab3eccdbcbb59cae
Venla R <venla.e.rantanen@gmail.com>
2023-11-08 14:48:57 +0200

    Task 9.5

--- osa9/index.ts
+++ osa9/index.ts
@@ -1,8 +1,25 @@
 import express from "express"
+import { calculateBmi } from "./bmiCalculator"
 const app = express()
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack")
+})
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+  if (isNaN(height) || isNaN(weight)) {
+    res.send({
+      error: "malformatted parameters",
+    })
+  }
+  const bmi = calculateBmi(height, weight)
+  res.send({
+    weight,
+    height,
+    bmi,
+  })
 })
 
 const PORT = 3003
