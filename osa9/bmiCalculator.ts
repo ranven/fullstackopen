5fda6ddf7760a6f444d885822e0c9169c106bf8d
Venla R <venla.e.rantanen@gmail.com>
2023-11-09 14:55:34 +0200

    Task 9.6

--- osa9/bmiCalculator.ts
+++ osa9/bmiCalculator.ts
@@ -1,30 +1,30 @@
 export const calculateBmi = (height: number, weight: number): string => {
-  const bmi = weight / ((height/100) ** 2) /* prettier-ignore */
+  const bmi = weight / ((height/100) ** 2); /* prettier-ignore */
   if (bmi < 18.5) {
-    return "Underweight (unhealthy weight)"
+    return "Underweight (unhealthy weight)";
   } else if (bmi >= 18.5 && bmi < 25) {
-    return "Normal (healthy weight)"
+    return "Normal (healthy weight)";
   } else if (bmi >= 25 && bmi < 30) {
-    return "Overweight (unhealthy weight)"
+    return "Overweight (unhealthy weight)";
   } else {
-    return "Obese (unhealthy weight)"
+    return "Obese (unhealthy weight)";
   }
-}
+};
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 if (args.length < 2) {
   console.log(
     "Too few arguments. Usage: npm run calculateBmi <height> <weight>"
-  )
+  );
 } else {
-  const height = Number(args[0])
-  const weight = Number(args[1])
+  const height = Number(args[0]);
+  const weight = Number(args[1]);
 
   if (isNaN(height) || isNaN(weight)) {
-    console.log("Provided values were not numbers")
+    console.log("Provided values were not numbers");
   } else {
-    const result = calculateBmi(height, weight)
-    console.log(result)
+    const result = calculateBmi(height, weight);
+    console.log(result);
   }
 }
--- osa9/exerciseCalculator.ts
+++ osa9/exerciseCalculator.ts
@@ -14,40 +14,40 @@ interface Rating {
 }
 
 const addRating = (target: number, total: number): Rating => {
-  const diffFromTarget = target - total
+  const diffFromTarget = target - total;
 
   if (diffFromTarget <= 1 && diffFromTarget >= -1) {
     return {
       rating: 1,
       ratingDescription: "Good job!",
-    }
+    };
   } else if (diffFromTarget <= 4 && diffFromTarget >= -4) {
     return {
       rating: 2,
       ratingDescription: "You're getting there!",
-    }
+    };
   } else {
     return {
       rating: 3,
       ratingDescription: "You could've done better...",
-    }
+    };
   }
-}
+};
 
 const calculateExercises = (hours: number[], target: number): Result => {
-  let trainingDays = 0
-  let periodLength = 0
-  let total = 0
+  let trainingDays = 0;
+  let periodLength = 0;
+  let total = 0;
 
   hours.forEach((h) => {
-    total += h
-    periodLength += 1
+    total += h;
+    periodLength += 1;
     if (h > 0) {
-      trainingDays += 1
+      trainingDays += 1;
     }
-  })
+  });
 
-  const rating = addRating(target, total)
+  const rating = addRating(target, total);
 
   return {
     periodLength,
@@ -57,29 +57,29 @@ const calculateExercises = (hours: number[], target: number): Result => {
     ratingDescription: rating.ratingDescription,
     target,
     average: total / periodLength,
-  }
-}
+  };
+};
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
 if (args.length < 2) {
   console.log(
     "Too few arguments. Usage: npm run calculateExercises <space-separated hours> <target>"
-  )
+  );
 } else {
-  let hoursArr: number[] = []
+  const hoursArr: number[] = [];
 
   for (let i = 1; i < args.length; i++) {
-    let n = args[i].split(" ").map(Number)
-    hoursArr.push(n[0])
+    const n = args[i].split(" ").map(Number);
+    hoursArr.push(n[0]);
   }
 
-  const target = Number(args[0])
+  const target = Number(args[0]);
 
   if (isNaN(target) || hoursArr.some(isNaN)) {
-    console.log("Provided values were not numbers")
+    console.log("Provided values were not numbers");
   } else {
-    const result = calculateExercises(hoursArr, target)
-    console.log(result)
+    const result = calculateExercises(hoursArr, target);
+    console.log(result);
   }
 }
